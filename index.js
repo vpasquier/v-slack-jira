a42c12c83cfa985546963bfa57077219c582c135
Vladimir Pasquier <vpasquier@nuxeo.com>
2017-08-24 19:52:14 -0400

    finalize jira link command

--- index.js
+++ index.js
@@ -2,66 +2,59 @@ const config = require("./config.json");
 const request = require("request");
 
 exports.handler = function (event, context) {
-    "use strict";
-    console.log("Start Lambda - Payload content:", event);
+  "use strict";
+  console.log("Start Lambda - Payload content:", event);
 
-    if (event.token !== config.token) {
-        return context.fail("Unauthorized request. This token is not valid:" + config.token);
+  if (event.token !== config.token) {
+    return context.fail("Unauthorized request. This token is not valid:" + config.token);
+  }
+
+  request(
+    "https://slack.com/api/channels.history?token=" + config.oauthToken + "&channel=" + event.channel_id,
+    function (error, response, body) {
+      if (!(error || !response)) {
+        if (response && response.statusCode !== 200) {
+          return context.fail("Cannot access to Slack team messages - Code:" + response.statusCode + " - " + response);
+        }
+      } else {
+        return context.fail("Cannot access to Slack team - " + error);
+      }
+
+      let json = JSON.parse(body);
+
+      if (!json.ok) {
+        context.fail("Cannot access to Slack team - " + json.error);
+      }
+
+      let jiraList = [];
+      json.messages.forEach(function (message) {
+        let text = message.text;
+        let matches = text.match(/(?:\s|^)([A-Z]+-[0-9]+)(?=\s|$)/g);
+        if (matches) {
+          matches.forEach(function (ticket) {
+            ticket = ticket.replace(/\s/g, '');
+            jiraList.push(config.jiraUrl + ticket);
+          });
+        }
+      });
+      let responseBody = {};
+      if (jiraList.length === 0) {
+        responseBody.text = ":jira:\nNo Jira ticket.\n";
+      } else {
+        responseBody.attachments = [];
+        responseBody.attachments[0] = {};
+        responseBody.attachments[0].pretext = ":jira:\n";
+        for (let i in jiraList) {
+          if (!responseBody.attachments[i]) {
+            responseBody.attachments[i] = {};
+          }
+          responseBody.attachments[i].title = jiraList[i];
+          responseBody.attachments[i].title_link = jiraList[i];
+        }
+      }
+      // For unit tests purpose
+      event.result = responseBody.attachments;
+      context.succeed(responseBody);
     }
-
-    request("https://slack.com/api/users.list?token=" + config.oauthToken, function (error, response, body) {
-        if (!(error || !response)) {
-            if (response && response.statusCode !== 200) {
-                return context.fail("Cannot access to Slack team users - Code:" + response.statusCode + " - " + response);
-            }
-        } else {
-            return context.fail("Cannot access to Slack team - " + error);
-        }
-
-        let json = JSON.parse(body);
-
-        if (!json.ok) {
-            context.fail("Cannot access to Slack team - " + json.error);
-        }
-
-        let statusList = [];
-
-        json.members.forEach(function (member) {
-            let profile = member.profile;
-            let bot = profile.is_bot || profile.bot_id;
-            if (!bot && profile.real_name !== "slackbot") {
-                let id = profile.status_emoji;
-                if (!id) {
-                    return;
-                }else{
-                    if(id.indexOf("flag")==-1) {
-                        return;
-                    }
-                }
-                if (!statusList[id]) {
-                    statusList[id] = [];
-                }
-                if (profile.status_text) {
-                    statusList[id].push(profile.real_name + " - " + profile.status_text);
-                } else {
-                    statusList[id].push(profile.real_name);
-                }
-            }
-        });
-
-        let responseBody = {};
-        responseBody.text = "";
-        for (var id in statusList) {
-            responseBody.text = responseBody.text + id + "\n";
-            statusList[id].forEach(function (user) {
-                responseBody.text = responseBody.text + user + "\n";
-            });
-            responseBody.text = responseBody.text + "\n";
-        }
-
-        // For unit tests purpose
-        event.result = responseBody.text;
-
-        context.succeed(responseBody);
-    });
+  );
 };
--- spec/lambda.spec.js
+++ spec/lambda.spec.js
@@ -3,54 +3,75 @@ const config = require("./config-test.json");
 const timeout = 3000;
 
 describe("Lambda Slack Jira", function () {
-    let lambda, context;
+  let lambda, context;
 
-    beforeAll(function () {
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-    });
+  beforeAll(function () {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  });
 
-    beforeEach(function (done) {
-        lambda = proxyquire("../index", {});
-        context = jasmine.createSpyObj("context", ["done", "succeed", "fail"]);
-        let evnt = {
-            token: "fail"
-        };
-        lambda.handler(evnt, context);
-        setTimeout(function () {
-            done();
-        }, timeout);
-    });
-    it("should return a token check failure", function (done) {
-        expect(context.fail).toHaveBeenCalled();
-        expect(context.succeed).not.toHaveBeenCalled();
-        done();
-    });
-
+  beforeEach(function (done) {
+    lambda = proxyquire("../index", {});
+    context = jasmine.createSpyObj("context", ["done", "succeed", "fail"]);
+    let evnt = {
+      token: "fail"
+    };
+    lambda.handler(evnt, context);
+    setTimeout(function () {
+      done();
+    }, timeout);
+  });
+  it("should return a token check failure", function (done) {
+    expect(context.fail).toHaveBeenCalled();
+    expect(context.succeed).not.toHaveBeenCalled();
+    done();
+  });
 });
 
 describe("Lambda Slack Jira", function () {
-    let lambda, context;
-    let evnt = {
-        token: config.token,
-        result: ""
-    };
+  let lambda, context;
+  let evnt = {
+    token: config.token,
+    result: ""
+  };
 
-    beforeAll(function () {
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-    });
+  beforeAll(function () {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  });
 
-    beforeEach(function (done) {
-        lambda = proxyquire("../index", {});
-        context = jasmine.createSpyObj("context", ["done", "succeed", "fail"]);
-        lambda.handler(evnt, context);
-        setTimeout(function () {
-            done();
-        }, timeout);
-    });
-    it("should succeed", function (done) {
-        expect(context.succeed).toHaveBeenCalled();
-        expect(context.fail).not.toHaveBeenCalled();
-        expect(evnt.result).toEqual(":flag-us:\nvlad letter - NYC\n\n");
-        done();
-    });
+  beforeEach(function (done) {
+    lambda = proxyquire("../index", {});
+    context = jasmine.createSpyObj("context", ["done", "succeed", "fail"]);
+    evnt.channel_id = "C03TPDMGS";
+    lambda.handler(evnt, context);
+    setTimeout(function () {
+      done();
+    }, timeout);
+  });
+  it("should succeed", function (done) {
+    expect(context.succeed).toHaveBeenCalled();
+    expect(context.fail).not.toHaveBeenCalled();
+    expect(evnt.result[0].title).toEqual("https://jira.nuxeo.com/browse/NXP-22963");
+    done();
+  });
+});
+
+describe("Lambda Slack Jira Matcher", function () {
+  beforeEach(function () {
+    lambda = proxyquire("../index", {});
+  });
+
+  it("should succeed", function () {
+    let text = "Bonjour, et oui, NXP-666 parce que pourquoi NXP-666 bah ouai, SUPNXP-323";
+    let jiraList = [];
+    let matches = text.match(/(?:\s|^)([A-Z]+-[0-9]+)(?=\s|$)/g);
+    if (matches) {
+      matches.forEach(function (ticket) {
+        ticket = ticket.replace(/\s/g, '');
+        jiraList.push(config.jiraUrl + ticket);
+      });
+    }
+    expect(jiraList[2]).toEqual(
+      "https://jira.nuxeo.com/browse/SUPNXP-323"
+    );
+  });
 });
